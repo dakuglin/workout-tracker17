adf8f9bcc920afa7c2ecb9820d7409275263076f
Dana Kuglin <dakuglin@gmail.com>
2020-11-14 12:33:45 -0700

    added files

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # workout-tracker17
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Description 
+
+
+
+
+## Table of Contents 
+
+* [Video](#video)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+
+
+## Video
+
+
+
+![Walkthrough Video](./.gif)
+
+
+## Installation
+
+Steps to install my project include:
+* npm install the dependencies 
+
+
+## Usage 
+
+![Getting Started](./pictures/.PNG)
+* 
+
+![Getting Started](./pictures/.PNG)
+* 
+
+
+## Credits
+
+All of the code was developed by Dana Kuglin. Information about her GitHub profile and how you can contact her are listed below in the questions section.  
+
+## License
+
+This project is licensed under MIT.
+
+## Questions
+
+GitHub Username: dakuglin
+GitHub Profile: (https://github.com/dakuglin)
+
+Please email me with additional questions at dakuglin@gmail.com  
---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose"); //require in mongoose
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercise: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter a workout name."
+      },
+      type: {
+        type: String,
+        required: "Please enter a workout type."
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "Please enter duration of workout (min)"
+      },
+    }
+  ]
+  
+});
+
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);  //telling mongoose we are going to name this model workout
+
+module.exports = Workout;
